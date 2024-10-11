56db073d579e77cbae4f9bfe2b21380ddc198e06
Daniel Lyon <daniellyon31@gmail.com>
2024-10-11 13:21:08 +1000

    paper submitted

--- scripts/merge_tsv.py
+++ scripts/merge_tsv.py
@@ -15,10 +15,10 @@ def read_zf_tsv(filename, skiprows, usecols):
     df = df.apply(pd.to_numeric, errors='coerce')
     return df
 
-# """ CDFS """
+""" CDFS """
 # # Get dataframes
-# cdfs = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS.tsv', skiprows=375, usecols=['Seq', 'Use'])
-# cdfs_sfr = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24'])
+# cdfs = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS.tsv', skiprows=375, usecols=['Seq', 'Use', 'RAJ2000', 'DEJ2000'])
+# cdfs_sfr = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24', 'SFR'])
 # cdfs_z = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS_Z.tsv', skiprows=80, usecols=['Seq', 'zpk'])
 # cdfs_flux = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS_FLUX.tsv', skiprows=117, usecols=['Seq', 'FU', 'FV', 'FJ'])
 # cdfs_xmatch = read_zf_tsv('DATA/ZFOURGE/CDFS/CDFS_xmatch.tsv', skiprows=54, usecols=['Seq', 'irAGN', 'radAGN', 'xAGN'])
@@ -26,12 +26,12 @@ def read_zf_tsv(filename, skiprows, usecols):
 
 # # Export to CSV
 # df = pd.concat(dataframes, axis=1)
-# df.to_csv('DATA/ZFOURGE/CDFS/CDFS_MAIN3.csv', sep=',')
+# df.to_csv('DATA/ZFOURGE/CDFS/CDFS_MAIN5.csv', sep=',')
 
-# """ COSMOS """
+""" COSMOS """
 # # Get dataframes
-# cosmos = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS.tsv', skiprows=354, usecols=['Seq', 'Use'])
-# cosmos_sfr = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24'])
+# cosmos = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS.tsv', skiprows=354, usecols=['Seq', 'Use', 'RAJ2000', 'DEJ2000'])
+# cosmos_sfr = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24', 'SFR'])
 # cosmos_z = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS_Z.tsv', skiprows=80, usecols=['Seq', 'zpk'])
 # cosmos_flux = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS_FLUX.tsv', skiprows=65, usecols=['Seq', 'FU', 'FV', 'FJ'])
 # cosmos_xmatch = read_zf_tsv('DATA/ZFOURGE/COSMOS/COSMOS_xmatch.tsv', skiprows=54, usecols=['Seq', 'irAGN', 'radAGN', 'xAGN'])
@@ -39,12 +39,12 @@ def read_zf_tsv(filename, skiprows, usecols):
 
 # # Export to CSV
 # df = pd.concat(dataframes, axis=1)
-# df.to_csv('DATA/ZFOURGE/COSMOS/COSMOS_MAIN3.csv', sep=',')
+# df.to_csv('DATA/ZFOURGE/COSMOS/COSMOS_MAIN5.csv', sep=',')
 
 """ UDS """
 # Get dataframes
-uds = read_zf_tsv('DATA/ZFOURGE/UDS/UDS.tsv', skiprows=286, usecols=['Seq', 'Use'])
-uds_sfr = read_zf_tsv('DATA/ZFOURGE/UDS/UDS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24'])
+uds = read_zf_tsv('DATA/ZFOURGE/UDS/UDS.tsv', skiprows=286, usecols=['Seq', 'Use', 'RAJ2000', 'DEJ2000'])
+uds_sfr = read_zf_tsv('DATA/ZFOURGE/UDS/UDS_SFR.tsv', skiprows=76, usecols=['Seq', 'LIR', 'F24', 'SFR'])
 uds_z = read_zf_tsv('DATA/ZFOURGE/UDS/UDS_Z.tsv', skiprows=80, usecols=['Seq', 'zpk'])
 uds_flux = read_zf_tsv('DATA/ZFOURGE/UDS/UDS_FLUX.tsv', skiprows=68, usecols=['Seq', 'FU', 'FV', 'FJ'])
 uds_xmatch = read_zf_tsv('DATA/ZFOURGE/UDS/UDS_xmatch.tsv', skiprows=54, usecols=['Seq', 'irAGN', 'radAGN', 'xAGN'])
@@ -52,4 +52,4 @@ dataframes = [uds, uds_sfr, uds_z, uds_flux, uds_xmatch]
 
 # Export to CSV
 df = pd.concat(dataframes, axis=1)
-df.to_csv('DATA/ZFOURGE/UDS/UDS_MAIN3.csv', sep=',')
+df.to_csv('DATA/ZFOURGE/UDS/UDS_MAIN5.csv', sep=',')
